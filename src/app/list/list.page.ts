414c6fd0fbde86590fec51020320dd394ac87864
jesusmen <jesusmengibardelom96@gmail.com>
2019-12-05 11:56:55 +0100

    pause inclose

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ANIMALES } from './data/data.animales';
+import { Animal } from './model/animales.interface';
 
 @Component({
   selector: 'app-list',
@@ -6,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+  animales: Animal[] = [];
+  audioAnimal = new Audio();
+  savedAnimal: any;
+  seAcabo: any;
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
 
-  constructor() { }
+  infoAnimal(info) {
+    console.log(info);
+  }
+  makeSound(animal) {
+    if (this.savedAnimal === undefined) {
+      this.audioAnimal.src = animal.audio;
+      this.audioAnimal.load();
+      this.audioAnimal.play();
+      animal.reproduciendo = true;
+      this.savedAnimal = animal;
+      setTimeout(function () {
+        animal.reproduciendo = false;
+        console.log(animal.reproduciendo);
+      }, (animal.duracion) * 1000);
+    }else{
+      this.savedAnimal.reproduciendo = false;
+      this.audioAnimal.currentTime = 0;
+      this.audioAnimal.pause();
 
+      this.audioAnimal.src = animal.audio;
+      this.audioAnimal.load();
+      this.audioAnimal.play();
+      animal.reproduciendo = true;
+      this.savedAnimal = animal;
+      this.seAcabo = setTimeout(function () {
+        animal.reproduciendo = false;
+        console.log(animal.reproduciendo);
+      }, (animal.duracion) * 1000);
+    }
+  }
+  stopSound(sound) {
+    this.audioAnimal.pause();
+    this.audioAnimal.currentTime = 0;
+    sound.reproduciendo = false;
+    clearTimeout(this.seAcabo);
+  }
+  deleteAnimal(i){
+    this.animales.splice(i, 1);
+  }
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      location.reload();
+      event.target.complete();
+    }, 2000);
+  }
   ngOnInit() {
   }
 
